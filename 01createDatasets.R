7f7dca29ff085a7ffc61eaeb365263dbe00c97e4
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-10-04 18:56:49 +0100

    transform features as factors

--- 01createDatasets.R
+++ 01createDatasets.R
@@ -3,27 +3,13 @@ library(biomaRt)
 
 ### options ###
 set.seed(16)
-human <- useMart("ensembl", "hsapiens_gene_ensembl")
+ensembl <- useMart("ensembl", "hsapiens_gene_ensembl")
 chr <- c(1:22, "X", "Y", "MT")
 #chr <- 21
 type="protein_coding"
 
 ### functions ###
-createBooleanFeature <- function(genes, attribute, mart=human) {
-    feat <- getBM(
-                  attributes=c("ensembl_gene_id", attribute),
-                  filters="ensembl_gene_id",
-                  values=genes,
-                  mart=mart)
-    feat[feat==""] <- NA
-    feat <- na.omit(feat)
-    feat$feat <- 1
-    feat <- reshape(feat, idvar="ensembl_gene_id", timevar=attribute, direction="wide")
-    feat <- merge(feat, genes, by="ensembl_gene_id", all=TRUE)
-    feat <- feat[order(feat$ensembl_gene_id), , drop=FALSE]
-}
-
-createNumericFeature <- function(genes, attribute, fun=median, mart=human) {
+createNumericFeature <- function(genes, attribute, fun=median, mart=ensembl) {
     feat <- getBM(
                   attributes=c("ensembl_gene_id", attribute),
                   filters="ensembl_gene_id",
@@ -35,56 +21,72 @@ createNumericFeature <- function(genes, attribute, fun=median, mart=human) {
     names(feat) <- c("ensembl_gene_id", attribute)
     feat <- merge(feat, genes, by="ensembl_gene_id", all=TRUE)
     feat <- feat[order(feat$ensembl_gene_id), , drop=FALSE]
+    feat[2:ncol(feat)] <- lapply(feat[2:ncol(feat)], as.numeric)
+    return(feat)
+}
+
+createFactorFeature <- function(genes, attribute, mart=ensembl) {
+    feat <- getBM(
+                  attributes=c("ensembl_gene_id", attribute),
+                  filters="ensembl_gene_id",
+                  values=genes,
+                  mart=mart)
+    feat[feat==""] <- NA
+    feat <- na.omit(feat)
+    feat$feat <- 1
+    feat <- reshape(feat, idvar="ensembl_gene_id", timevar=attribute, direction="wide")
+    feat <- merge(feat, genes, by="ensembl_gene_id", all=TRUE)
+    feat <- feat[order(feat$ensembl_gene_id), , drop=FALSE]
+    feat[2:ncol(feat)] <- lapply(feat[2:ncol(feat)], as.factor)
+    return(feat)
 }
 
 ### data ###
-
 # get all protein coding genes
 genes <- getBM(
                attributes="ensembl_gene_id",
                filters=c("chromosome_name", "biotype"),
                values=list(chr, type),
-               mart=human)
+               mart=ensembl)
 genes <- genes[order(genes$ensembl_gene_id), , drop=FALSE]
 
-# create numeric and boolean features
+# create numeric and factor features
 transcript_length <- createNumericFeature(genes, "transcript_length")
 transcript_count <- createNumericFeature(genes, "transcript_count")
 percentage_gc_content <- createNumericFeature(genes, "percentage_gc_content")
-low_complexity <- createBooleanFeature(genes, "low_complexity")
-transmembrane_domain <- createBooleanFeature(genes, "transmembrane_domain")
-signal_domain <- createBooleanFeature(genes, "signal_domain")
-ncoils <- createBooleanFeature(genes, "ncoils")
-interpro <- createBooleanFeature(genes, "interpro")
-#go_id <- createBooleanFeature(genes, "go_id")
-goslim_goa_accession <- createBooleanFeature(genes, "goslim_goa_accession")
-reactome <- createBooleanFeature(genes, "reactome")
 mmusculus_homolog_perc_id <- createNumericFeature(genes, "mmusculus_homolog_perc_id")
 drerio_homolog_perc_id <- createNumericFeature(genes, "drerio_homolog_perc_id")
 dmelanogaster_homolog_perc_id <- createNumericFeature(genes, "dmelanogaster_homolog_perc_id")
 celegans_homolog_perc_id <- createNumericFeature(genes, "celegans_homolog_perc_id")
 scerevisiae_homolog_perc_id <- createNumericFeature(genes, "scerevisiae_homolog_perc_id")
+low_complexity <- createFactorFeature(genes, "low_complexity")
+transmembrane_domain <- createFactorFeature(genes, "transmembrane_domain")
+signal_domain <- createFactorFeature(genes, "signal_domain")
+ncoils <- createFactorFeature(genes, "ncoils")
+interpro <- createFactorFeature(genes, "interpro")
+go_id <- createFactorFeature(genes, "go_id")
+reactome <- createFactorFeature(genes, "reactome")
 
 # generate complete set with all features
 completeset <- cbind(genes,
                  transcript_length[2:ncol(transcript_length)],
                  transcript_count[2:ncol(transcript_count)],
                  percentage_gc_content[2:ncol(percentage_gc_content)],
+                 mmusculus_homolog_perc_id[2:ncol(mmusculus_homolog_perc_id)],
+                 drerio_homolog_perc_id[2:ncol(drerio_homolog_perc_id)],
+                 dmelanogaster_homolog_perc_id[2:ncol(dmelanogaster_homolog_perc_id)],
+                 celegans_homolog_perc_id[2:ncol(celegans_homolog_perc_id)],
+                 scerevisiae_homolog_perc_id[2:ncol(scerevisiae_homolog_perc_id)],
                  low_complexity[2:ncol(low_complexity)],
                  transmembrane_domain[2:ncol(transmembrane_domain)],
                  signal_domain[2:ncol(signal_domain)],
                  ncoils[2:ncol(ncoils)],
                  interpro[2:ncol(interpro)],
-                 #go_id[2:ncol(go_id)],
-                 goslim_goa_accession[2:ncol(goslim_goa_accession)],
-                 reactome[2:ncol(reactome)],
-                 mmusculus_homolog_perc_id[2:ncol(mmusculus_homolog_perc_id)],
-                 drerio_homolog_perc_id[2:ncol(drerio_homolog_perc_id)],
-                 dmelanogaster_homolog_perc_id[2:ncol(dmelanogaster_homolog_perc_id)],
-                 celegans_homolog_perc_id[2:ncol(celegans_homolog_perc_id)],
-                 scerevisiae_homolog_perc_id[2:ncol(scerevisiae_homolog_perc_id)]
+                 go_id[2:ncol(go_id)],
+                 reactome[2:ncol(reactome)]
                  )
 completeset[is.na(completeset)] <- 0
+names(completeset) <- gsub("[-:]", "", names(completeset))
 
 # read targetpedia data for target information
 targetpedia <- read.delim("/GWD/bioinfo/projects/bix-analysis-stv/data/pharmaceutical/targetpedia/targetpedia_triples.txt")
@@ -92,7 +94,7 @@ targetpedia <- getBM(
                  attributes="ensembl_gene_id",
                  filters=c("entrezgene", "chromosome_name", "biotype"),
                  values=list(targetpedia$Target_EntrezGeneId, chr, type),
-                 mart=human)
+                 mart=ensembl)
 
 # read pharmaprojects data for target information
 pharmaprojects <- read.delim("/GWD/bioinfo/projects/bix-analysis-stv/data/pharmaceutical/pipeline/pipeline_triples.txt")
@@ -100,7 +102,7 @@ pharmaprojects <- getBM(
                         attributes="ensembl_gene_id",
                         filters=c("entrezgene", "chromosome_name", "biotype"),
                         values=list(pharmaprojects$Target_EntrezGeneId, chr, type),
-                        mart=human)
+                        mart=ensembl)
 
 # positive cases: these are targets according to targetpedia and/or pharmaprojects
 positive <- unique(rbind(targetpedia, pharmaprojects))
@@ -115,6 +117,7 @@ unknown$target <- 0
 # dataset is made of positive and unknown cases
 # will be splitted into traing and test sets
 dataset <- rbind(positive, unknown)
+dataset$target <- as.factor(dataset$target)
 saveRDS(dataset, file.path("../data/dataset.rds"))
 
 # prediction set will be kept for the actual prediction
