cb048a0781e69903fa13a30eb15bb9e02651f32a
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-10-05 09:53:00 +0100

    feature selection

--- 02classifyDatasets.R
+++ 02classifyDatasets.R
@@ -1,14 +1,34 @@
+### libraries ###
 library(mlr)
-set.seed(16)
+library(parallelMap)
 
-# data
+### options ###
+set.seed(16)
+parallelStart("multicore", 16)
+
+
+### data ###
 dataset <- readRDS(file.path("../data/dataset.rds"))
 
-# mlr setup
-classif.task <- makeClassifTask(id="TargetPred", data=dataset[-1], target="target")
-svm.learner <- makeLearner("classif.svm", predict.type="response")
-bagged.svm.learner <- makeBaggingWrapper(svm.learner, bw.iters=10)
-resampl.desc <- makeResampleDesc("CV", iters=3)
+### setup ###
+## task
+classif.task <- makeClassifTask(id="TargetPred", data=dataset, target="target")
 
-# model
-mod <- resample(bagged.svm.learner, classif.task, resampl.desc)
+## learner
+svm.lrn <- makeLearner("classif.svm")
+bag.svm.lrn <- makeBaggingWrapper(svm.lrn, bw.iters=10)
+bag.svm.lrn <- setPredictType(bag.svm.lrn, predict.type="prob")
+
+## resampling description
+rdesc <- makeResampleDesc("CV", iters=3)
+
+## feature selection tuning
+lrn <- makeFilterWrapper(bagged.svm.learner, fw.method="information.gain")
+ps <- makeParamSet(makeDiscreteParam("fw.perc", values = c(0.5, 0.25, 0.1, 0.05, 0.01, 0.001)))
+res <- tuneParams(lrn, classif.task, rdesc, par.set=ps, control=makeTuneControlGrid())
+
+## feature selection
+bag.svm.lrn <- makeFilterWrapper(bag.svm.lrn, fw.method="information.gain", fw.perc=res$x$fw.perc)
+
+## model with resampling
+mod <- resample(bag.svm.lrn, classif.task, rdesc)
