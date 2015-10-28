628677d620e711a1a30da87473a0283572367bed
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-10-28 17:46:49 +0000

    add gene annotation

--- 03predictTargets.R
+++ 03predictTargets.R
@@ -1,15 +1,29 @@
 ### libraries ###
 library(mlr)
 library(parallelMap)
+library(biomaRt)
 
 ### options ###
 set.seed(16)
 parallelStart("multicore", 16)
+ensembl <- useMart("ensembl", "hsapiens_gene_ensembl")
+chr <- c(1:22, "X", "Y", "MT")
+type="protein_coding"
 
 ### data ###
-predicitonset <- readRDS(file.path("../data/predicitionset.rds"))
+predictionset <- readRDS(file.path("../data/predicitionset.rds"))
 mod <- readRDS(file.path("../data/mod.rds"))
 
 ### predict ###
-pred <- predict(mod, newdata=predicitonset)
+pred <- predict(mod, newdata=predictionset)
 saveRDS(pred, file.path("../data/pred.rds"))
+
+### annotate ###
+ann <- getBM(attributes=c("ensembl_gene_id", "entrezgene", "external_gene_name"),
+             filters=c("ensembl_gene_id", "chromosome_name", "biotype"),
+             values=list(rownames(pred$data), chr, type),
+             mart=ensembl)
+predres <- merge(pred$data, ann, by.x="row.names", by.y="ensembl_gene_id", all=TRUE)
+names(predres) <- c("Ensembl", "UnknownProb", "TargetProb", "Prediction", "Entrez", "Symbol")
+predres <- predres[c("Ensembl", "Entrez", "Symbol", "Prediction", "TargetProb", "UnknownProb")]
+write.csv(predres, file.path("../data/PredicitonResults.csv"), quote=FALSE, row.names=FALSE)
