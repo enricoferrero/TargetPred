07fab2cf1e3e7703f6f9c92101723741c520d5d2
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2015-11-20 11:14:00 +0000

    annotate features

---
+++ 04annotateFeatures.R
@@ -0,0 +1,49 @@
+library(biomaRt)
+#library(reactome.db)
+#ensembl <- useMart("ensembl", "hsapiens_gene_ensembl")
+ensembl <- useMart("ENSEMBL_MART_ENSEMBL", "hsapiens_gene_ensembl", host="www.ensembl.org")
+
+# separate small molecules and antibodies
+for (agenttype in c("small_molecule", "antibody")) {
+
+    fv <- readRDS(file.path(paste0("../data/fv.", agenttype, ".rds")))
+    fv <- fv$data[order(fv$data[[filter.method]], decreasing=TRUE),]
+    fv$name <- sub("feat.", "", fv$name)
+    fv$name <- sub("GO_", "GO:", fv$name)
+    fv$name <- sub("R_HSA_", "R-HSA-", fv$name)
+
+    interpro <- fv[grep("^IPR", fv$name), "name"]
+    interpro <- getBM(attributes=c("interpro", "interpro_description"), filters="interpro", values=interpro, mart=ensembl)
+    names(interpro) <- c("name", "description")
+    interpro <- na.omit(merge(fv, interpro, all=TRUE))
+
+    go_id <- fv[grep("^GO", fv$name), "name"]
+    go_id <- getBM(attributes=c("go_id", "name_1006"), filters="go_id", values=go_id, mart=ensembl)
+    names(go_id) <- c("name", "description")
+    go_id <- na.omit(merge(fv, go_id, all=TRUE))
+
+    #reactome <- fv[grep("^R-HSA", fv$name), "name"]
+    #reactome <- getBM(attributes=c("reactome", "reactome_description"), filters="reactome", values=reactome, mart=ensembl)
+    #names(reactome) <- c("name", "description")
+    #reactome <- na.omit(merge(fv, reactome, all=TRUE))
+
+    others <- fv[grep("^(?!IPR)", fv$name, perl=TRUE), ]
+    others <- others[grep("^(?!GO)", others$name, perl=TRUE), ]
+    others <- others[grep("^(?!R-HSA)", others$name, perl=TRUE), ]
+    others$description <- others$name
+
+    #feats <- rbind(interpro, go_id, reactome, others)
+    feats <- rbind(interpro, go_id, others)
+    feats <- feats[order(feats[3], decreasing=TRUE), ]
+    write.csv(feats, file.path(paste0("../data/SelectedFeatures.", agenttype, ".csv")), quote=FALSE, row.names=FALSE)
+
+}
+
+
+
+
+
+
+
+
+
