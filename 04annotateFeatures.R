65437bd558d1a49fa7d16cfa6827e82a62c7623e
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2016-03-01 09:55:43 +0000

    add EFO annotation

--- 04annotateFeatures.R
+++ 04annotateFeatures.R
@@ -19,15 +19,22 @@ go_id <- na.omit(merge(fv, go_id, all=TRUE))
 
 reactann <- read.delim("http://www.reactome.org/download/current/ReactomePathways.txt", header=FALSE)
 reactome <- fv[grep("^R-HSA", fv$name), "name"]
-reactome <- unique(merge(reactome, reactann[1:2], by=1))
+reactome <- unique(merge(reactome, reactann[1:2], by=1, all.x=TRUE, all.y=FALSE))
 names(reactome) <- c("name", "description")
 reactome <- na.omit(merge(fv, reactome, all=TRUE))
 
-others <- fv[grep("^(?!IPR)", fv$name, perl=TRUE), ]
-others <- others[grep("^(?!GO)", others$name, perl=TRUE), ]
-others <- others[grep("^(?!R-HSA)", others$name, perl=TRUE), ]
+efoann <- read.csv("/GWD/bioinfo/projects/bix-analysis-stv/data/CTTV/v2.0/matrix.csv.gz")
+efoann <- efoann[c("OntologyId", "Label")]
+efo <- fv[grep("^genetic_association|^somatic_mutation|^rna_expression|^affected_pathway|^animal_model", fv$name), "name"]
+efo <- as.data.frame(do.call(rbind, strsplit(efo, "\\.")))
+efo <- unique(merge(efo, efoann, by.x=2, by.y=1, all.x=TRUE, all.y=FALSE))
+efo$name <- paste0(efo[[2]], ".", efo[[1]])
+efo$description <- paste0(efo[[3]], " ", "(", efo[[2]], ")")
+efo <- na.omit(merge(fv, efo[c("name", "description")], all=TRUE))
+
+others <- fv[grep("^IPR|^GO|R-HSA|^genetic_association|^somatic_mutation|^rna_expression|^affected_pathway|^animal_model", fv$name, invert=TRUE), ]
 others$description <- others$name
 
-feats <- rbind(interpro, go_id, reactome, others)
+feats <- rbind(interpro, go_id, reactome, efo, others)
 feats <- feats[order(feats[3], decreasing=TRUE), ]
 write.csv(feats, file.path("../data/SelectedFeatures.csv"), quote=TRUE, row.names=FALSE)
