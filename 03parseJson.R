2f019803afc97a288fb1bcb6614f1c30c732f0af
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2016-06-10 15:55:39 +0100

    include more fields

--- 03parseJson.R
+++ 03parseJson.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 
 dat <- stream_in(gzfile("/GWD/bioinfo/projects/bix-analysis-stv/data/CTTV/v3.0/16.04_association_data.json.gz"))
-dat <- cbind.data.frame(dat$target$id, dat$disease$id, dat$is_direct, dat$association_score$datatypes)
-names(dat) <- c("EnsemblId", "OntologyId", "Is.direct", "literature", "rna_expression", "genetic_association", "somatic_mutation", "known_drug", "animal_model", "affected_pathway")
-write.csv(dat, gzfile("/GWD/bioinfo/projects/bix-analysis-stv/data/CTTV/v3.0/public.matrix.csv.gz"), quote=FALSE, row.names=FALSE)
+dat <- cbind.data.frame(dat$target$id, dat$target$gene_info$symbol, dat$disease$id, dat$disease$efo_info$label, dat$is_direct, dat$association_score$overall, dat$association_score$datatypes)
+names(dat) <- c("EnsemblId", "Symbol", "OntologyId", "Label", "Is.direct", "overall", "literature", "rna_expression", "genetic_association", "somatic_mutation", "known_drug", "animal_model", "affected_pathway")
+write.csv(dat, gzfile("/GWD/bioinfo/projects/bix-analysis-stv/data/CTTV/v3.0/public.matrix.csv.gz"), row.names=FALSE)
