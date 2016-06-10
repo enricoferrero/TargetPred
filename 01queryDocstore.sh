db2fd64b9607a319030e335570411c6feb7d2bfe
Enrico Ferrero <enrico.x.ferrero@gsk.com>
2016-06-10 15:56:18 +0100

    only use gene target hits from DocStore

--- 01queryDocstore.sh
+++ 01queryDocstore.sh
@@ -2,6 +2,3 @@
 
 ## gene_target
 curl -H 'Accept:text/tab-separated-values' 'http://us1salx00648.corpnet2.com:9999/api/ds/v1/search/co/sentence/sentencedetail/flat/*/*/*/*?terms=type%3AGENE%20id%3ABIOPROC%24BP71232&inorder=false&slop=1000&limit=100000&from=0&sortby=document_date%3Adesc' > ../data/docstore.gene_target.results.txt
-
-## gene_target_indication
-curl -H 'Accept:text/tab-separated-values' 'http://us1salx00648.corpnet2.com:9999/api/ds/v1/search/co/sentence/sentencedetail/flat/*/*/*/*?terms=type%3AGENE%20id%3ABIOPROC%24BP71232%20type%3AINDICATION&inorder=false&slop=1000&limit=100000&from=0&sortby=document_date%3Adesc' > ../data/docstore.gene_target_indication.results.txt
